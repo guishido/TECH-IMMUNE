081a08525395086288a02f8a02d79796a367f6c7
Guilherme <83133709+guishido@users.noreply.github.com>
2023-11-11 11:11:34 -0300

    update 5

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function mostrarTexto() {
         textoEscondido.style.display = 'none';
     }
 }
+
+function mostrarTexto() {
+    var textoEscondido = document.getElementById('texto-escondido1');
+    if (textoEscondido.style.display === 'none') {
+        textoEscondido.style.display = 'inline';
+    } else {
+        textoEscondido.style.display = 'none';
+    }
+}
